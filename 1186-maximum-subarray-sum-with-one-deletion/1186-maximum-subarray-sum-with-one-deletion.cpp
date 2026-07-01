9bd577436a64f4f1560f290e305c936ce215ba09
Sak17shii-code <sakshiigupta17@gmail.com>
2026-07-01 23:15:18 +0530

    Time: 0 ms (100%), Space: 27 MB (95.63%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
+        
         int res = arr[0];
         int nodel = arr[0];
-        int onedel = INT_MIN;
+        int onedel = 0;
         int n = arr.size();
 
         for(int i = 1; i < n; i++){
-
             int prevnodel = nodel;
             int prevonedel = onedel;
-            nodel = max(nodel+arr[i],arr[i]);
-        if (prevonedel == INT_MIN)
-            onedel = prevnodel;
-        else
-            onedel = max(prevnodel, prevonedel + arr[i]);
-            res = max(res,max(nodel,onedel));
-            
+
+            nodel = max(prevnodel + arr[i], arr[i]);
+            onedel = max(prevnodel , prevonedel + arr[i]);
+
+            res = max(res, max(nodel, onedel));
+
         }
         return res;
+
         
     }
 };
